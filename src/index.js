a122559d94e75ed18fc4dccadb4e9e3293f572e5
dat nguyen <nguyenvdat257@gmail.com>
2022-06-30 16:06:09 +0700

    add redux persist

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Provider } from 'react-redux';
 import store from "./store";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor = persistStore(store);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pageSlice from "./page-slice";
 import authSlice from "./auth-slice";
+import signupSlice from "./signup-slice";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {combineReducers} from "redux"; 
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const reducers =  combineReducers({
+    page: pageSlice.reducer,
+    auth: authSlice.reducer,
+    signup: signupSlice.reducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-    reducer: {
-        page: pageSlice.reducer,
-        auth: authSlice.reducer,
-    }
+    reducer: persistedReducer
 });
 
 export default store
