cefd344e9bc95277f924d7420786fb7e6f636ad5
dat nguyen <nguyenvdat257@gmail.com>
2022-08-18 13:19:34 +0700

    edit config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const myConfig = { 
     // hostName:'http://127.0.0.1:8000',
-    hostName:'https://social-net-216.herokuapp.com/',
+    hostName:'https://social-net-216.herokuapp.com',
     // socketHostName:'ws://127.0.0.1:8000',
-    socketHostName:'ws://social-net-216.herokuapp.com/',
+    socketHostName:'ws://social-net-216.herokuapp.com',
     defaultAvatar: 'http://social-net-188.herokuapp.com/default_avatar.jpg',
     defaultHashtag: 'http://social-net-188.herokuapp.com/default_hashtag.png',
     getError: 'Cannot get data from server.',
